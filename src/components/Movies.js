1caeda071e1819a237516f97a037088a0e892b5e
azeelleeza <leezaluncheon@gmail.com>
2018-05-21 16:12:07 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, x) => (
+        <div key={x}>
+          <h2>{movie.title}</h2>
+          <h4>{movie.time}</h4>
+          <ul>{movie.genres.map((genre, y) => <li key={y}>{genre}</li>)}</ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+const active = {
+  color: "green"
+};
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink to="/" exact activeStyle={active}>
+        Home
+      </NavLink>
+
+      <NavLink to="/movies" activeStyle={active}>
+        Movies
+      </NavLink>
+
+      <NavLink to="/actors" activeStyle={active}>
+        Actors
+      </NavLink>
+
+      <NavLink to="/directors" activeStyle={active}>
+        Directors
+      </NavLink>
     </div>
   );
 };
